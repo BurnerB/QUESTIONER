f22d2e72d79f0147f592fbe24f958fff99cbb91b
Barnaby <barnabywaweru@hotmail.com>
2019-02-01 11:21:37 +0300

    [feature  #163582106] added __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+'''Creating app'''
+import os
+from flask import Flask
+from instance.config import app_config
+"""importing the configurations from the .config file which is in the instance folder"""
+
+def create_app(config_name):
+    '''creating  the app using the configurations in the dictionary created in the .config file'''
+    app = Flask(__name__)
+    return app
