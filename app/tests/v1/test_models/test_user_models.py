6b7b097718e0533e01395ef7288418b55867a69d
Barnaby <barnabywaweru@hotmail.com>
2019-02-05 23:04:24 +0300

    [feature  #163721441] modified test

--- app/tests/v1/test_models/test_user_models.py
+++ app/tests/v1/test_models/test_user_models.py
@@ -1,24 +1,44 @@
 """Test user model file"""
 
 import unittest
-# from app.api.v1.models.user_models import user_models
+from app.api.v1.models.user_models import User
+
 
 class TestUser(unittest.TestCase):
     """Test Case for user model"""
     def setUp(self):
-        User.user_list=[]
+        user_id = 1
+        firstname = "John"
+        lastname = "Doe"
+        email = "johndoe@gmail.com"
+        phone_number = "0272988632"
+        username = "Johny"
+        password = "baba123"
+        confirmpassword = "baba123"
+        registered  =  "4 27/2/19"
+        isAdmin =  False
+        user=User(user_id ,firstname,lastname,email,phone_number ,username ,password ,confirmpassword ,registered ,isAdmin)
+        User.user_list.append(user)
+
     
     def test_create_user(self):
         """Test if a user is added to the data structure"""
-        user = User.create_user()
+        User.user_list = []
+        firstname = "John"
+        lastname = "Doe"
+        email = "johndoe@gmail.com"
+        phone_number = "0272988632"
+        username = "Johny"
+        password = "baba123"
+        confirmpassword = "baba123"
+        User.create_user(firstname,lastname,email,phone_number ,username ,password ,confirmpassword)
         self.assertEqual(1,len(User.user_list))
-        self.assertTrue(user,dict)
+    
 
     def test_delete_user(self):
         """Test if a user is successfully deleted"""
-        user1 = User.create_user()
-        self.assertEqual(1,len(User.user_list))
-        user=User.delete_user(1)
+        result = User.delete_user(1)
+        self.assertTrue(result)
         self.assertEqual(0,len(User.user_list))
 
     def tearDown(self):
