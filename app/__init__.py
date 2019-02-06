374ffad9aae00bab37f6b23e18bead0dccf6d189
Barnaby <barnabywaweru@hotmail.com>
2019-02-06 08:31:22 +0300

    fix failing travis

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from app.instance.config import app_config
 
 """importing the configurations from the .config file which is in the instance folder"""
 
-from app.api.v1.endpoints.user_endpoints import version_1 as user_v1
+from api.v1.endpoints.user_endpoints import version_1 as user_v1
 
 def create_app(config_name):
     '''creating  the app using the configurations in the dictionary created in the .config file'''
