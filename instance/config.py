45f8c5fae0223b783689ed979e9efd62e008514e
Barnaby <barnabywaweru@hotmail.com>
2019-02-06 08:28:08 +0300

    fix failing travis

---
+++ instance/config.py
@@ -0,0 +1,38 @@
+"""The project configuration files."""
+import os
+
+
+class Config():
+    """Parent configuration."""
+
+    DEBUG = False
+    TESTING = False
+    SECRET_KEY = os.getenv('SECRET_KEY')
+
+
+class Development(Config):
+    """Development environment config."""
+
+    DEBUG = True
+    TESTING = True
+
+
+class Production(Config):
+    """Production environment config."""
+
+    DEBUG = False
+    TESTING = False
+
+
+class Testing(Config):
+    """Testing environment config."""
+
+    DEBUG = True
+    TESTING = True
+
+
+app_config = {
+    'development': Development,
+    'testing': Testing,
+    'production"': Production
+}
