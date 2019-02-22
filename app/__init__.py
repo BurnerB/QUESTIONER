a8fd84297e65245b21edc69af121b49dc88e7209
Barnaby <barnabywaweru@hotmail.com>
2019-02-22 13:34:43 +0300

    [start #164027503] modified init

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ from datetime import timedelta
 """importing the configurations from the .config file which is in the instance folder"""
 
 from app.api.v1.endpoints.user_endpoints import version_1 as user_v1
+from app.api.v1.endpoints.meetup_endpoints import version_1 as meetup_v1
 
 def create_app(config_name):
     '''creating  the app using the configurations in the dictionary created in the .config file'''
@@ -18,4 +19,5 @@ def create_app(config_name):
 
     
     app.register_blueprint(user_v1)
+    app.register_blueprint(meetup_v1)
     return app
