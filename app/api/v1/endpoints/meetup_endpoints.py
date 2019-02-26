6fb40d0cec1ced048825a24b6d7f0ba0d0be20fd
Barnaby <barnabywaweru@hotmail.com>
2019-02-26 17:01:37 +0300

    [start #164166100] modified endpoints

--- app/api/v1/endpoints/meetup_endpoints.py
+++ app/api/v1/endpoints/meetup_endpoints.py
@@ -2,6 +2,10 @@ from flask import Flask, Blueprint, request ,jsonify ,make_response
 import datetime
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
+from app.api.v1.utils.validators import Validators
+
+Validators = Validators()
+
 """ local imports"""
 from app.api.v1.models.meetup_models import meetup_list, Meetup
 MeetupModel = Meetup()
@@ -11,6 +15,7 @@ version_1 = Blueprint("Meetups" ,__name__)
 
 
 @version_1.route("/meetups",methods=["POST"])
+@jwt_required
 def create_meetup():
     try:
         data=request.get_json()
@@ -25,7 +30,30 @@ def create_meetup():
             return jsonify({"status":400,
                             "error" :"Invalid {} key field".format(e)
                                     }),400
-        
+
+    if not Validators.check_topic(topic):
+        return jsonify ({"status":400,
+                            "error":"Topic contains invalid characters"}),400
+    
+    if not Validators.check_location(location):
+        return jsonify ({"status":400,
+                        "error":"Location contains invalid characters"}),400
+
+
+    try:
+        datetime.datetime.strptime(happening_On, '%d/%m/%Y')
+    except Exception as e:
+        return jsonify({"status":400,
+                        "error" :"Incorrect data format, should be DD/MM/YYYY"
+                                }),400
+    
+    # if not Validators.check_hapenningOn_date(happening_On):
+    #     return jsonify ({"status":400,
+    #                     "error":"Date has already passed"}),400
+    
+    if Validators.check_image == None:
+        return jsonify ({"status":400,
+                        "error":"Unsupported image type"}),400
     
     if len(topic)== 0 or len(location) == 0 or len(happening_On) == 0 or len(tags)== 0 or len(image) == 0:
         return jsonify({"message":"cannot be an empty string"}),400
