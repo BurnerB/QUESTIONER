1a443da3e60623fd52ab87c3ce5039c7588147cb
Barnaby <barnabywaweru@hotmail.com>
2019-02-27 19:05:42 +0300

    [feature #164166100] modified endpoints

--- app/api/v1/models/meetup_models.py
+++ app/api/v1/models/meetup_models.py
@@ -1,12 +1,11 @@
 """This module defines the meetup models and associated functions"""
+from flask import jsonify, abort, json
 from datetime import datetime
-
-meetup_list = []
 
 class Meetup(object):
     """This class encapsulates the functions of the meetup model"""
     def __init__(self):
-        self.db = meetup_list
+        self.db = []
    
     def create_meetup(self, topic ,location ,happening_On ,tags ,image):
         payload = {
@@ -29,10 +28,11 @@ class Meetup(object):
         for meetup in self.db:
             if meetup["meetup_id"] == meetup_id:
                 return meetup
+        return False
     
     def delete_meetup(self, meetup_id):
-        meetup = self.get_meetup(meetup_id)
-        if meetup == None:
-            return False
-        meetup_list.remove(meetup)
-        return True
+        Meetup = [Meetup for Meetup in self.db if Meetup['meetup_id'] == meetup_id]
+        if Meetup:
+            self.db.remove(Meetup[0])
+            return True
+        return False
